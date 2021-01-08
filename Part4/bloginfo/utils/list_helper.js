e9532ed7c02e4ac26ae14cc8b4f1a49339904703
James <james.hallam93@gmail.com>
2021-01-08 14:55:04 +0000

    mostblogs function

--- Part4/bloginfo/utils/list_helper.js
+++ Part4/bloginfo/utils/list_helper.js
@@ -1,3 +1,5 @@
+const { indexOf } = require("lodash")
+const lodash = require("lodash")
 
 
 const dummy = (array) =>{
@@ -16,11 +18,10 @@ const totalLikes = (array) =>{
 }
 
 const favoriteBlog = (array) =>{
+
+    //must be a way of doing this without empty object constant - why does returning empty object (return {})  evaluate to a string?
     const empty = {}
     let favorite = {
-        title:"",
-        author:"",
-        url:"",
         likes:-1
     }
     array.map(blog =>{
@@ -33,4 +34,37 @@ const favoriteBlog = (array) =>{
     }
     return favorite
 }
-module.exports = {dummy, totalLikes, favoriteBlog}
+
+const mostBlogs = (array) =>{
+    let authors = []
+    let numberOfBlogs = []
+
+
+    array.map(blog =>{
+        if (!authors.includes(blog.author)){
+            authors.push(blog.author)
+            numberOfBlogs.push(1)  
+
+        }else if(authors.includes(blog.author)){
+            const index = authors.indexOf(blog.author)
+            numberOfBlogs[index] = numberOfBlogs[index] + 1
+    }})
+
+    const indexOfGreatest = numberOfBlogs.reduce((maxIndex, currentValue, currentIndex, arrayObject) =>{
+        return (currentValue > arrayObject[maxIndex]?
+            currentIndex :
+            maxIndex
+        )}
+    )
+
+    return {
+        author: authors[indexOfGreatest],
+        blogs: numberOfBlogs[indexOfGreatest]
+    }
+}
+
+const blogs = [ { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 }, { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 }, { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 }, { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 }, { _id: "5a422ba71b54a676234d17fb", title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0, __v: 0 }, { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }]
+
+console.log(mostBlogs(blogs))
+
+module.exports = {dummy, totalLikes, favoriteBlog, mostBlogs}
